bfac03a1a9aefdf4bd6d61c67a44d030c8907125
priyanka7777777 <sarafpriyanka434@gmail.com>
2024-01-17 23:34:55 +0530

    sorting technique

---
+++ javaprogramminglanguage/Sorting.java
@@ -0,0 +1,88 @@
+import java.util.*;
+public class Sorting{
+    public static void bubblesort(int[]arr){
+        for(int i =0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                
+                if(arr[j]<arr[j+1]){
+                    //swap
+int temp =arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
+
+                }
+            }
+        }
+
+    }
+    public static void SelectionSort(int[]arr){
+        for(int i=0;i<arr.length-1;i++){
+            int minpos=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minpos]<arr[j]){
+                    minpos=j;
+                }
+            }
+            int temp=arr[minpos];
+            arr[minpos]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    public static void printarr(int [] arr){
+    for(int i =0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+}
+  public static void InsertionSort(int[] arr) {
+        for (int i = 1; i <= arr.length - 1; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+    
+            // Finding correct position to insert
+            while (prev <= 0 && arr[prev] < curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+    }
+     public static void Countingsort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            largest= Math.max(largest,arr[i]);
+        }
+        int count[]=new int [largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        //sorting 
+        int j=0;
+        for(int i=0;i<count.length;i++){
+            while(count[i]<0){
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }
+        }
+     }
+    public static void main(String args[]){
+int [] arr= {1,2,4,3,5}; 
+ bubblesort(arr);
+ System.out.println("Sorted array using bubble sort:");
+printarr(arr);
+
+SelectionSort(arr);
+System.out.println("Sorted array using selection sort:");
+printarr(arr);
+
+InsertionSort(arr);
+System.out.println("Sorted array using Insertionsort sort:");
+printarr(arr);
+
+Countingsort(arr);
+System.out.println("Sorted array using counting sort:");
+printarr(arr);
+
+    }
+    
+}
